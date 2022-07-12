eeb93a5c6f7f9821f85209167202bf8e3d8ed024
TheMo-at <iam@themo.at>
2022-07-12 10:03:22 +0200

    up                                                                                                                                    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-function popup() {
-    alert("Testing ist Funny");
+const display = $('#display .inner');
+const digitButtons = $('td[data-digit]');
+
+
+function clickDigit() {
+    const digit = $(this).data('digit');
+
+    if (display.html() === '0') {
+    display.text(digit);
+    }
+    else {
+        display.append(digit);
 }
+}
+
+digitButtons.click(clickDigit);
